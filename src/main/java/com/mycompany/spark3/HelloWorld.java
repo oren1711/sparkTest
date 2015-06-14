3bc5f37811f9286e94c3e0cee192a0e99f77be9c
oren1711 <oren1711@gmail.com>
2015-06-14 19:47:13 +0300

    init

---
+++ src/main/java/com/mycompany/spark3/HelloWorld.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.spark3;
+
+import static spark.Spark.*;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        get("/hello", (request, response) -> "i love nofar!!");
+        get("/nofar", (request, response) -> "i love nofar!!");
+        get("/sayHi/:name", (request, response) -> "hi "+request.params(":name"));
+    }
+}
